d6f3aab2969d4f8a992cc48f22a3d6074c8350c4
Antoine Guiot <you@example.com>
2020-03-01 14:35:20 +0100

    maj

--- data_preprocess.py
+++ data_preprocess.py
@@ -1,6 +1,9 @@
 import cv2
 import numpy as np
 import pandas as pd
+import skimage
+
+from skimage.feature import hog
 
 
 class dataFormatter:
@@ -14,10 +17,12 @@ class dataFormatter:
 
     def compute_hog(self):
         hist = []
-        # image = cv2.imread("test.jpg",0)
+        hog_features = []
+        hog_images = []
+
         winSize = (48, 48)
-        blockSize = (16, 16)
-        blockStride = (8, 8)
+        blockSize = (20, 20)
+        blockStride = (10, 10)
         cellSize = (8, 8)
         nbins = 9
         derivAperture = 1
@@ -26,15 +31,18 @@ class dataFormatter:
         L2HysThreshold = 2.0000000000000001e-01
         gammaCorrection = 0
         nlevels = 64
-        hog = cv2.HOGDescriptor(winSize, blockSize, blockStride, cellSize, nbins, derivAperture, winSigma,
-                                histogramNormType, L2HysThreshold, gammaCorrection, nlevels)
+        #hog = cv2.HOGDescriptor(winSize, blockSize, blockStride, cellSize, nbins, derivAperture, winSigma,
+        #                        histogramNormType, L2HysThreshold, gammaCorrection, nlevels)
         # compute(img[, winStride[, padding[, locations]]]) -> descriptors
+
         winStride = (8, 8)
         padding = (8, 8)
         locations = ((10, 20),)
 
         for image in self.images:
-            hist.append(hog.compute(image))
+            features, hog_image = hog(image, orientations=8, pixels_per_cell=(16, 16),
+                                      cells_per_block=(1, 1), visualise=True)
+            hog_features.append(features)
+            hog_images.append(hog_image)
 
-        return hist
-
+        return hog_features, hog_image
