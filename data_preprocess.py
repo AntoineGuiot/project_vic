dbd31697ba55f4209051fcb9d03a6f303d581629
Antoine Guiot <you@example.com>
2020-03-01 16:37:21 +0100

    add model.py modif

--- data_preprocess.py
+++ data_preprocess.py
@@ -6,38 +6,21 @@ import skimage
 from skimage.feature import hog
 from skimage import data, exposure
 
+from sklearn.preprocessing import OneHotEncoder
+
+
 class dataFormatter:
     def __init__(self, csv):
-        self.csv = csv
-        images = self.csv.pixels.values
+        self.data = csv
+        images = self.data.pixels.values
         for i, image in enumerate(images):
-            images[i] = np.array([int(i) for i in self.csv.pixels.values[i].split()], dtype=np.uint8).reshape((48, 48))
+            images[i] = np.array([int(i) for i in self.data.pixels.values[i].split()], dtype=np.uint8).reshape((48, 48))
 
         self.images = images
 
     def compute_hog(self):
-        hist = []
         hog_features = []
         hog_images = []
-
-        winSize = (48, 48)
-        blockSize = (20, 20)
-        blockStride = (10, 10)
-        cellSize = (8, 8)
-        nbins = 9
-        derivAperture = 1
-        winSigma = 4.
-        histogramNormType = 0
-        L2HysThreshold = 2.0000000000000001e-01
-        gammaCorrection = 0
-        nlevels = 64
-        #hog = cv2.HOGDescriptor(winSize, blockSize, blockStride, cellSize, nbins, derivAperture, winSigma,
-        #                        histogramNormType, L2HysThreshold, gammaCorrection, nlevels)
-        # compute(img[, winStride[, padding[, locations]]]) -> descriptors
-
-        winStride = (8, 8)
-        padding = (8, 8)
-        locations = ((10, 20),)
 
         for image in self.images:
             features, hog_image = hog(image, orientations=8, pixels_per_cell=(8, 8),
@@ -45,8 +28,12 @@ class dataFormatter:
             hog_features.append(features)
             hog_images.append(hog_image)
 
-        return hog_features, hog_images
+        self.data['hog_features'] = hog_features
 
+        return hog_images
 
-
-
+    def process_target(self):
+        enc = OneHotEncoder(handle_unknown='ignore', sparse=False)
+        emotion = enc.fit_transform(self.data.emotion.values.reshape(-1, 1))
+        for i in range(emotion.shape[1]):
+            self.data[enc.categories_[0][i]] = emotion[:, i]
--- model.py
+++ model.py
@@ -1,3 +1,39 @@
 import sklearn
 import numpy as np
 import pandas as pd
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+
+class Model:
+
+    def __init__(self, model_type=None, hyperparameters=None, predictors=None, targets=None):
+        self.model_type = model_type
+
+        self.hyperparameters = hyperparameters
+        self.predictors = predictors
+        self.targets = targets
+
+    def load(self, path):
+        return
+
+    def save(self, path):
+        return
+
+    def create_model(self):
+        if self.model_type == 'SVM':
+            self.model = SVC(random_state=self.hyperparameters['random_state'],
+                             max_iter=self.hyperparameters['epochs'],
+                             kernel=self.hyperparameters['kernel'],
+                             decision_function_shape=self.hyperparameters['decision_function'],
+                             gamma=self.hyperparameters['gamma'])
+
+    def train(self, train_set):
+        self.model.fit(train_set[self.predictors], train_set[self.targets])
+
+    def test(self, test_set):
+        prediction = self.predict(test_set[self.predictors])
+        return accuracy_score(test_set[self.targets], prediction)
+
+    def predict(self, X):
+        return self.model.predict(X)
